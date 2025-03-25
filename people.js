0448063eb3e31c06751492711cbedc0371e0721b
AlbertoCadorin <alberto.cadori01@gmail.com>
2025-03-25 10:18:49 +0100

    fix: function person

--- people.js
+++ people.js
@@ -1,14 +1,13 @@
 // Importa la tua funzione da names.js
-const fullName = require("./names.js")
+const CreateName = require("./names.js")
 // Importa la tua funzione da hobbies.js
-const personHobbies = require("./hobbies.js")
+const hobbies = require("./hobbies.js")
 //  Crea una funzione che non ha parametri. La funzione dovrebbe restituire un oggetto con due proprietà: fullName ed hobbies. All'interno della tua funzione, usa le tue due funzioni precedenti per costruire l'oggetto.
 function person() {
-    const name = fullName('Alberto','Cadorin')
-    const hobbies = personHobbies('Video Games', 'Cucinare','Guardare Anime')
+    
     return {
-        fullName:`${name.firstName} ${name.lastName}`,
-        hobbies: hobbies
+        fullName : CreateName('Alberto','Cadorin'),
+        personHobbies: hobbies('Video Games', 'Cucinare','Guardare Anime')
 
     }
 }
